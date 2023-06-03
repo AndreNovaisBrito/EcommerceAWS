8252fa276abf0e9d83b60c8238d7185e20169687
AndreNovaisBrito <andrenoz@gmail.com>
2023-06-03 11:37:12 +0100

    feat: log getProductId

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -2,30 +2,37 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
-} from "aws-lambda";
+} from "aws-lambda"
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  const lambdaRequestId = context.awsRequestId;
-  const apiRequestId = event.requestContext.requestId;
+  const lambdaRequestId = context.awsRequestId
+  const apiRequestId = event.requestContext.requestId
 
   console.log(
     `API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`
-  );
+  )
 
-  const method = event.httpMethod;
+  const method = event.httpMethod
   if (event.resource === "/products") {
     if (method === "GET") {
-      console.log("GET");
+      console.log("GET")
 
       return {
         statusCode: 200,
         body: JSON.stringify({
           message: "GET Products - OK",
         }),
-      };
+      }
+    }
+  } else if (event.resource === "/products/{id}") {
+    const productId = event.pathParameters!.id as string
+    console.log(`GET /products/${productId}`)
+    return {
+      statusCode: 200,
+      body: `GET /products/${productId}`,
     }
   }
 
@@ -34,5 +41,5 @@ export async function handler(
     body: JSON.stringify({
       message: "Bad request",
     }),
-  };
+  }
 }
